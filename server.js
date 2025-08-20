7ad85e3b8c080dd59c07a0f9a108d5824386f92e
unknown <a.shataie2023@gmail.com>
2025-08-19 23:06:29 -0700

    chore: Create DB connection

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const { ApolloServer } = require("apollo-server-express");
 // * Create App
 const app = express();
 
+// * ---- Mongoose Connection ----
+mongoose.connect("mongodb://127.0.0.1/graphql-movie-api");
+mongoose.connection.once("open", () => {
+  console.log(`Connect to DB successfully`);
+});
+
 // * ---- Listening ----
 app.listen(3000, "127.0.0.1", () =>
     console.log(`Server running on localhost:3000`)
