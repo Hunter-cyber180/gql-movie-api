47ca680242895ae0f5388def7424c2a55ec87a58
unknown <a.shataie2023@gmail.com>
2025-08-19 23:15:04 -0700

    feat: Create user model

---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    phoneNumber: { type: Number, required: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("User", UserSchema);
