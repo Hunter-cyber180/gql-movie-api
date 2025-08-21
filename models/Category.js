d1f85ffbe5288ac61caa498967b93b4d54ceaee6
unknown <a.shataie2023@gmail.com>
2025-08-20 23:28:28 -0700

    feat: Create category model

---
+++ models/Category.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
