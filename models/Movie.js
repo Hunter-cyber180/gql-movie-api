6c02e50759ef321bd7b7a1ac0443e6638472834f
unknown <a.shataie2023@gmail.com>
2025-08-22 10:53:21 -0700

    feat: Add timestamps to movie model

--- models/Movie.js
+++ models/Movie.js
@@ -11,6 +11,6 @@ const MovieSchema = new Schema({
     director: { type: String, required: true },
     trailerSrc: { type: String, required: true },
     views: { type: Number, required: true }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Movie", MovieSchema);
