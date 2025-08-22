159e56fb25a9c0c889e015975a3500d6683cb29f
unknown <a.shataie2023@gmail.com>
2025-08-22 10:50:12 -0700

    feat: Create movie schema

---
+++ models/Movie.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const MovieSchema = new Schema({
+    name: { type: String, required: true },
+    desc: { type: String, required: true },
+    src: { type: String, required: true },
+    releaseYear: { type: Number, required: true },
+    duration: { type: Number, required: true },
+    genres: [{ type: String, required: true }],
+    director: { type: String, required: true },
+    trailerSrc: { type: String, required: true },
+    views: { type: Number, required: true }
+});
