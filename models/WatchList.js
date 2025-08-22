2603a859b6d35bf51aa55b4d8d0fffb66782aee3
unknown <a.shataie2023@gmail.com>
2025-08-22 11:12:40 -0700

    feat: Create watch list movies schema

--- models/WatchList.js
+++ models/WatchList.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const WatchListMoviesSchema = new Schema({
+    movieID: { type: mongoose.Types.ObjectId, ref: "Movie", required: true },
+    addedAt: { type: Date, required: true },
+    watched: { type: Boolean, default: false },
+});
+
 const WatchListSchema = new Schema({
     name: { type: String, required: true },
     user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    movies: [WatchListMoviesSchema],
 }, { timestamps: true });
