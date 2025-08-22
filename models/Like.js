2cfe424937ab54e3684848de3efd5f9426a96f36
unknown <a.shataie2023@gmail.com>
2025-08-22 10:59:22 -0700

    refactor: Add required to like model fields

--- models/Like.js
+++ models/Like.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const LikeSchema = new Schema({
-    movie: { type: mongoose.Types.ObjectId, ref: "Movie" },
-    user: { type: mongoose.Types.ObjectId, ref: "User" },
+    movie: { type: mongoose.Types.ObjectId, ref: "Movie", required: true },
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
 }, {
     timestamps: true
 });
