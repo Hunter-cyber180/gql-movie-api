b3a0df6a30e7f1a6e42446b8e16fcdded179db8a
unknown <a.shataie2023@gmail.com>
2025-08-24 13:12:23 -0700

    feat: Create Reply Comment model

---
+++ models/ReplyComment.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ReplyCommentSchema = new Schema({
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    movie: { type: mongoose.Types.ObjectId, ref: "Movie", required: true },
+    comment: { type: mongoose.Types.ObjectId, ref: "Comment" },
+    body: { type: String, required: true },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 }
+}, { timestamps: true });
+
+module.exports = mongoose.model("ReplyComment", ReplyCommentSchema);
