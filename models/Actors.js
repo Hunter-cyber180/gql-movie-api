430c369e56b09f36d999ce670f3273c5642311b1
unknown <a.shataie2023@gmail.com>
2025-08-23 23:11:49 -0700

    refactor: Change birth type to Date in actor model

--- models/Actors.js
+++ models/Actors.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const ActorSchema = new Schema({
     fullname: { type: String, required: true },
     bio: { type: String, default: null },
-    DateOfBirth: { type: Number, required: true },
+    DateOfBirth: { type: Date, required: true },
     PlaceOfBirth: { type: String, required: true },
     ProfileImageURL: { type: String, default: null }
 }, { timestamps: true });
