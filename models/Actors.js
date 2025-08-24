57c1cef28ac1fc6983d16de54337f3b22c77c1e2
unknown <a.shataie2023@gmail.com>
2025-08-23 23:06:42 -0700

    feat: Create actor schema

---
+++ models/Actors.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ActorSchema = new Schema({
+    fullname: { type: String, required: true },
+    bio: { type: String, default: null },
+    DateOfBirth: { type: Number, required: true },
+    PlaceOfBirth: { type: String, required: true },
+    ProfileImageURL: { type: String, default: null }
+}, { timestamps: true });
