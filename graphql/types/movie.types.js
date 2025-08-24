69b7856e0abcaf970a7614bba57945f2e7fb3d17
unknown <a.shataie2023@gmail.com>
2025-08-24 13:23:55 -0700

    feat: Create movie input

--- graphql/types/movie.types.js
+++ graphql/types/movie.types.js
@@ -13,3 +13,19 @@ exports.Movie = `
     rating: Number
   }
 `;
+
+exports.MovieInput = `
+  input MovieInput {
+    name: String,
+    desc: String,
+    src: String,
+    releaseYear: Number,
+    duration: Number,
+    genres: [String],
+    director: String,
+    trailerSrc: String,
+    views: Number,
+    country: String,
+    rating: Number
+  }
+`;
