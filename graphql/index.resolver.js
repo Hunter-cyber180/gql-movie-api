e568aa620b5c54842ee02b93e01bf734cb321e55
unknown <a.shataie2023@gmail.com>
2025-08-25 20:58:09 -0700

    feat: Add category queries to root resolver

--- graphql/index.resolver.js
+++ graphql/index.resolver.js
@@ -1,5 +1,11 @@
+// * ---- Resolvers ----
+const likeResolvers = require("./resolvers/like.resolvers");
+const categoryResolvers = require("./resolvers/category.resolvers");
+
 const RootResolvers = {
     Query: {
+        categories: categoryResolvers.categories,
+        category: categoryResolvers.category,
     },
 
     Mutation: {
