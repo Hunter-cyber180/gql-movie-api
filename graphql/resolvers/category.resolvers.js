bc907d3ca8ef2f36defb43ca1f0b9ba335a56b53
unknown <a.shataie2023@gmail.com>
2025-08-25 20:47:07 -0700

    feat: Create get a single category resolver

--- graphql/resolvers/category.resolvers.js
+++ graphql/resolvers/category.resolvers.js
@@ -3,6 +3,9 @@ const CategoryModel = require("../../models/Category");
 
 const categories = async () => await CategoryModel.find({});
 
+const category = async ({ id: _id }) => await CategoryModel.findOne({ _id });
+
 module.exports = {
     categories,
+    category,
 };
