3f040e8a7575ca9130850631f1bd7c8d6b28070b
unknown <a.shataie2023@gmail.com>
2025-08-25 20:54:24 -0700

    feat: Create get a single like resolver

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -3,6 +3,9 @@ const LikeModel = require("../../models/Like");
 
 const likes = async () => await LikeModel.find({});
 
+const like = async ({ id: _id }) => await LikeModel.findOne({ _id });
+
 module.exports = {
     likes,
+    like,
 };
