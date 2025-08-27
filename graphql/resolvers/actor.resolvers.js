cd356c7690eb9e74d430887961c3f6824ff5cad0
unknown <a.shataie2023@gmail.com>
2025-08-26 21:03:28 -0700

    feat: Create get a single actor resolver

--- graphql/resolvers/actor.resolvers.js
+++ graphql/resolvers/actor.resolvers.js
@@ -3,6 +3,9 @@ const ActorModel = require("../../models/Actor");
 
 const actors = async () => await ActorModel.find({});
 
+const actor = async ({ id: _id }) => await ActorModel.findOne({ _id });
+
 module.exports = {
     actors,
+    actor,
 };
