ab6861c3f2473eecbeae5a2039b58998937e134f
unknown <a.shataie2023@gmail.com>
2025-08-26 20:46:53 -0700

    feat: Create get a single article resolver

--- graphql/resolvers/article.resolvers.js
+++ graphql/resolvers/article.resolvers.js
@@ -3,6 +3,9 @@ const ArticleModel = require("../../models/Article");
 
 const articles = async () => await ArticleModel.find({});
 
+const article = async ({ id: _id }) => await ArticleModel.findOne({ _id });
+
 module.exports = {
     articles,
+    article,
 };
