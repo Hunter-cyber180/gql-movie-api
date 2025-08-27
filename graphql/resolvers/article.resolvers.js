d0ce1e11923d8f87d06ca1d64008c2882e784d66
unknown <a.shataie2023@gmail.com>
2025-08-26 20:49:33 -0700

    feat: Create add article resolver

--- graphql/resolvers/article.resolvers.js
+++ graphql/resolvers/article.resolvers.js
@@ -5,7 +5,16 @@ const articles = async () => await ArticleModel.find({});
 
 const article = async ({ id: _id }) => await ArticleModel.findOne({ _id });
 
+const addArticle = async (_, args, context) => {
+    const { author, imageSrc, title, body, likes, dislikes } = args;
+    // TODO => check user role (the author of article)
+    return await ArticleModel.create({
+        author, imageSrc, title, body, likes, dislikes
+    });
+}
+
 module.exports = {
     articles,
     article,
+    addArticle,
 };
