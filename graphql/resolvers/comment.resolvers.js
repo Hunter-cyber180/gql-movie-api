9dcd324788c1e66dbf22a3c3ac7c35e423aadbcd
unknown <a.shataie2023@gmail.com>
2025-08-27 23:52:17 -0700

    feat: Create add comment resolver

--- graphql/resolvers/comment.resolvers.js
+++ graphql/resolvers/comment.resolvers.js
@@ -5,7 +5,13 @@ const comments = async () => await CommentModel.find({});
 
 const comment = async ({ id: _id }) => await CommentModel.findOne({ _id });
 
+const addComment = async (_, args) => {
+    const { user, movie, body, likes, dislikes } = args;
+    return await CommentModel.create({ user, movie, body, likes, dislikes });
+};
+
 module.exports = {
     comments,
     comment,
+    addComment,
 };
