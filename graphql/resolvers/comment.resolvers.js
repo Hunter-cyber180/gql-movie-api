7d591a2e5ea517c265376ce5953509a6c824c2e6
unknown <a.shataie2023@gmail.com>
2025-08-27 23:50:24 -0700

    feat: Create get a single comment resolver

--- graphql/resolvers/comment.resolvers.js
+++ graphql/resolvers/comment.resolvers.js
@@ -3,6 +3,9 @@ const CommentModel = require("../../models/Comment");
 
 const comments = async () => await CommentModel.find({});
 
+const comment = async ({ id: _id }) => await CommentModel.findOne({ _id });
+
 module.exports = {
     comments,
+    comment,
 };
