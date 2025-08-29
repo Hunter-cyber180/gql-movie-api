16f29eae7b2c114a34c0b8a9da6f7129cb819410
unknown <a.shataie2023@gmail.com>
2025-08-28 23:49:21 -0700

    feat: Create get a single movie resolver

--- graphql/resolvers/movie.resolvers.js
+++ graphql/resolvers/movie.resolvers.js
@@ -3,6 +3,9 @@ const MovieModel = require("../../models/Movie");
 
 const movies = async () => await MovieModel.find({});
 
+const movie = async ({ id: _id }) => await  MovieModel.findOne({ _id });
+
 module.exports = {
     movies,
+    movie,
 };
