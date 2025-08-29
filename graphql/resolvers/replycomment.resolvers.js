76861230f307e62f508c6feeccead13ca332e134
unknown <a.shataie2023@gmail.com>
2025-08-28 23:43:00 -0700

    feat: Create add reply comment resolver

--- graphql/resolvers/replycomment.resolvers.js
+++ graphql/resolvers/replycomment.resolvers.js
@@ -5,7 +5,15 @@ const replyComments = async () => await ReplyCommentModel.find({});
 
 const replyComment = async ({ id: _id }) => await ReplyCommentModel.findOne({ _id });
 
+const addReplyComment = async (_, args) => {
+    const { user, movie, comment, body, likes, dislikes } = args;
+    return await ReplyCommentModel.create(
+        { user, movie, comment, body, likes, dislikes }
+    );
+};
+
 module.exports = {
     replyComments,
     replyComment,
+    addReplyComment
 };
