64cf53544f6821b70ab7a31b0a6510accf12445f
unknown <a.shataie2023@gmail.com>
2025-08-28 23:40:56 -0700

    feat: Create get a single reply comment resolver

--- graphql/resolvers/replycomment.resolvers.js
+++ graphql/resolvers/replycomment.resolvers.js
@@ -3,6 +3,9 @@ const ReplyCommentModel = require("../../models/ReplyComment");
 
 const replyComments = async () => await ReplyCommentModel.find({});
 
+const replyComment = async ({ id: _id }) => await ReplyCommentModel.findOne({ _id });
+
 module.exports = {
     replyComments,
+    replyComment,
 };
