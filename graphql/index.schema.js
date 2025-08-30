3ccb2b68c1a51d196bd21b2cd7409e5577d0e0f1
unknown <a.shataie2023@gmail.com>
2025-08-29 23:27:00 -0700

    feat: Add category types to index schema

--- graphql/index.schema.js
+++ graphql/index.schema.js
@@ -1,8 +1,11 @@
 const articleTypes = require("./types/article.types");
+const categoryTypes = require("./types/category.types");
 
 module.exports = `#graphql
     ${articleTypes.Article}
     ${articleTypes.ArticleInput}
+    ${categoryTypes.Category}
+    ${categoryTypes.CategoryInput}
 
     type Query {
 
