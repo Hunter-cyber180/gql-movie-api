ad9610df62b57eab323bc3a74c7caab062af0909
unknown <a.shataie2023@gmail.com>
2025-08-29 23:27:42 -0700

    feat: Add like types to index schema

--- graphql/index.schema.js
+++ graphql/index.schema.js
@@ -1,11 +1,14 @@
 const articleTypes = require("./types/article.types");
 const categoryTypes = require("./types/category.types");
+const likeTypes = require("./types/like.types");
 
 module.exports = `#graphql
     ${articleTypes.Article}
     ${articleTypes.ArticleInput}
     ${categoryTypes.Category}
     ${categoryTypes.CategoryInput}
+    ${likeTypes.Like}
+    ${likeTypes.LikeInput}
 
     type Query {
 
