77b2a38b458e627999d7b1d07e46cb5a15a2d30a
unknown <a.shataie2023@gmail.com>
2025-09-04 23:44:42 -0700

    feat: Add user validation to comment schema

---
+++ utils/validators/comment.validate.js
@@ -0,0 +1,12 @@
+const Schema = require("validate");
+
+const commentSchema = new Schema({
+    user: {
+        type: String,
+        required: true,
+        validate: {
+            validator: (v) => mongoose.Types.ObjectId.isValid(v),
+            message: "userID must be a valid MongoDB ObjectId!"
+        }
+    },
+});
