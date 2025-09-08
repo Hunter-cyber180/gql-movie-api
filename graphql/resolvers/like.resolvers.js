2892e8746c23fbe862ebe559b088eed0b03534b0
unknown <a.shataie2023@gmail.com>
2025-09-07 23:44:29 -0700

    feat: Validate data in add like resolver

--- graphql/resolvers/like.resolvers.js
+++ graphql/resolvers/like.resolvers.js
@@ -1,12 +1,16 @@
 // * ---- Models ----
 const LikeModel = require("../../models/Like");
 
+// * ---- Utils ----
+const { likeValidator } = require("../../utils/validators/like.validate");
+
 const likes = async () => await LikeModel.find({});
 
 const like = async ({ id: _id }) => await LikeModel.findOne({ _id });
 
 const addLike = async (_, args, context) => {
     const { user, food } = args;
+    await likeValidator(context.req);
     return await LikeModel.create({ user, food });
 }
 
