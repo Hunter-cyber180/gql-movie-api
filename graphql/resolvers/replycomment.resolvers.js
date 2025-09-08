63ef38514b928df07212b12ea0933c2a641ab1f6
unknown <a.shataie2023@gmail.com>
2025-09-07 23:47:38 -0700

    feat: Validate data in add reply comment resolver

--- graphql/resolvers/replycomment.resolvers.js
+++ graphql/resolvers/replycomment.resolvers.js
@@ -1,12 +1,16 @@
 // * ---- Models ----
 const ReplyCommentModel = require("../../models/ReplyComment");
 
+// * ---- Utils ----
+const { replyCommentValidator } = require("../../utils/validators/replycomment.validate");
+
 const replyComments = async () => await ReplyCommentModel.find({});
 
 const replyComment = async ({ id: _id }) => await ReplyCommentModel.findOne({ _id });
 
-const addReplyComment = async (_, args) => {
+const addReplyComment = async (_, args, context) => {
     const { user, movie, comment, body, likes, dislikes } = args;
+    await replyCommentValidator(context.req);
     return await ReplyCommentModel.create(
         { user, movie, comment, body, likes, dislikes }
     );
