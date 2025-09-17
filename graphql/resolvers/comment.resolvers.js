b7684fe6ba0cfae067e79ad8ebc567f29592a43b
unknown <a.shataie2023@gmail.com>
2025-09-16 23:53:45 -0700

    refactor: Refactor get comment codes

--- graphql/resolvers/comment.resolvers.js
+++ graphql/resolvers/comment.resolvers.js
@@ -14,7 +14,17 @@ const comments = async () => {
     }
 }
 
-const comment = async ({ id: _id }) => await CommentModel.findOne({ _id });
+const comment = async ({ id: _id }) => {
+    try {
+        const comment = await CommentModel.findOne({ _id });
+        if (!comment)
+            throw new Error("Comment not found!");
+
+        return comment;
+    } catch (error) {
+        throw new Error(`Error fetching comment: ${error.message}`);
+    }
+}
 
 const addComment = async (_, { input }, context) => {
     const { user, movie, body, likes, dislikes } = input;
