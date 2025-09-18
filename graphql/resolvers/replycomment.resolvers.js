0833d30dbacf177f277033a9c2be54664ca619c2
unknown <a.shataie2023@gmail.com>
2025-09-17 23:42:01 -0700

    refactor: Refactor get reply comments codes

--- graphql/resolvers/replycomment.resolvers.js
+++ graphql/resolvers/replycomment.resolvers.js
@@ -5,7 +5,14 @@ const ReplyCommentModel = require("../../models/ReplyComment");
 const { replyCommentValidator } = require("../../utils/validators/replycomment.validate");
 const { authValidator, adminValidator } = require("../../utils/auth");
 
-const replyComments = async () => await ReplyCommentModel.find({});
+const replyComments = async () => {
+    try {
+        const replyComments = await ReplyCommentModel.find({});
+        return replyComments;
+    } catch (error) {
+        throw new Error(`Error fetching reply comments: ${error.message}`);
+    }
+}
 
 const replyComment = async ({ id: _id }) => await ReplyCommentModel.findOne({ _id });
 
